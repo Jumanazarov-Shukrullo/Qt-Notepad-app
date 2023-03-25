fc726f184783b87b459c045da02a9d90a3c03802
Jumanazarov-Shukrullo <shukrullo.coder@gmail.com>
2023-03-25 06:08:59 +0500

    added release files

---
+++ Notepad-Desktop-Release/moc_mainwindow.cpp
@@ -0,0 +1,241 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../notepad/Notepad/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[33];
+    char stringdata0[473];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 10), // "action_new"
+QT_MOC_LITERAL(2, 22, 0), // ""
+QT_MOC_LITERAL(3, 23, 17), // "action_new_window"
+QT_MOC_LITERAL(4, 41, 11), // "action_open"
+QT_MOC_LITERAL(5, 53, 11), // "action_save"
+QT_MOC_LITERAL(6, 65, 14), // "action_save_as"
+QT_MOC_LITERAL(7, 80, 12), // "action_print"
+QT_MOC_LITERAL(8, 93, 11), // "action_exit"
+QT_MOC_LITERAL(9, 105, 11), // "action_undo"
+QT_MOC_LITERAL(10, 117, 10), // "action_cut"
+QT_MOC_LITERAL(11, 128, 11), // "action_copy"
+QT_MOC_LITERAL(12, 140, 12), // "action_paste"
+QT_MOC_LITERAL(13, 153, 13), // "action_delete"
+QT_MOC_LITERAL(14, 167, 11), // "action_find"
+QT_MOC_LITERAL(15, 179, 17), // "action_select_all"
+QT_MOC_LITERAL(16, 197, 16), // "action_date_time"
+QT_MOC_LITERAL(17, 214, 11), // "action_font"
+QT_MOC_LITERAL(18, 226, 12), // "action_color"
+QT_MOC_LITERAL(19, 239, 23), // "action_background_color"
+QT_MOC_LITERAL(20, 263, 32), // "action_backgroud_color_edit_text"
+QT_MOC_LITERAL(21, 296, 14), // "action_zoom_in"
+QT_MOC_LITERAL(22, 311, 15), // "action_zoom_out"
+QT_MOC_LITERAL(23, 327, 19), // "action_default_zoom"
+QT_MOC_LITERAL(24, 347, 17), // "action_status_bar"
+QT_MOC_LITERAL(25, 365, 16), // "action_view_help"
+QT_MOC_LITERAL(26, 382, 20), // "action_send_feedback"
+QT_MOC_LITERAL(27, 403, 20), // "action_about_notepad"
+QT_MOC_LITERAL(28, 424, 8), // "findNext"
+QT_MOC_LITERAL(29, 433, 3), // "str"
+QT_MOC_LITERAL(30, 437, 19), // "Qt::CaseSensitivity"
+QT_MOC_LITERAL(31, 457, 2), // "cs"
+QT_MOC_LITERAL(32, 460, 12) // "findPrevious"
+
+    },
+    "MainWindow\0action_new\0\0action_new_window\0"
+    "action_open\0action_save\0action_save_as\0"
+    "action_print\0action_exit\0action_undo\0"
+    "action_cut\0action_copy\0action_paste\0"
+    "action_delete\0action_find\0action_select_all\0"
+    "action_date_time\0action_font\0action_color\0"
+    "action_background_color\0"
+    "action_backgroud_color_edit_text\0"
+    "action_zoom_in\0action_zoom_out\0"
+    "action_default_zoom\0action_status_bar\0"
+    "action_view_help\0action_send_feedback\0"
+    "action_about_notepad\0findNext\0str\0"
+    "Qt::CaseSensitivity\0cs\0findPrevious"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      28,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  154,    2, 0x0a /* Public */,
+       3,    0,  155,    2, 0x0a /* Public */,
+       4,    0,  156,    2, 0x0a /* Public */,
+       5,    0,  157,    2, 0x0a /* Public */,
+       6,    0,  158,    2, 0x0a /* Public */,
+       7,    0,  159,    2, 0x0a /* Public */,
+       8,    0,  160,    2, 0x0a /* Public */,
+       9,    0,  161,    2, 0x0a /* Public */,
+      10,    0,  162,    2, 0x0a /* Public */,
+      11,    0,  163,    2, 0x0a /* Public */,
+      12,    0,  164,    2, 0x0a /* Public */,
+      13,    0,  165,    2, 0x0a /* Public */,
+      14,    0,  166,    2, 0x0a /* Public */,
+      15,    0,  167,    2, 0x0a /* Public */,
+      16,    0,  168,    2, 0x0a /* Public */,
+      17,    0,  169,    2, 0x0a /* Public */,
+      18,    0,  170,    2, 0x0a /* Public */,
+      19,    0,  171,    2, 0x0a /* Public */,
+      20,    0,  172,    2, 0x0a /* Public */,
+      21,    0,  173,    2, 0x0a /* Public */,
+      22,    0,  174,    2, 0x0a /* Public */,
+      23,    0,  175,    2, 0x0a /* Public */,
+      24,    0,  176,    2, 0x0a /* Public */,
+      25,    0,  177,    2, 0x0a /* Public */,
+      26,    0,  178,    2, 0x0a /* Public */,
+      27,    0,  179,    2, 0x0a /* Public */,
+      28,    2,  180,    2, 0x08 /* Private */,
+      32,    2,  185,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 30,   29,   31,
+    QMetaType::Void, QMetaType::QString, 0x80000000 | 30,   29,   31,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->action_new(); break;
+        case 1: _t->action_new_window(); break;
+        case 2: _t->action_open(); break;
+        case 3: _t->action_save(); break;
+        case 4: _t->action_save_as(); break;
+        case 5: _t->action_print(); break;
+        case 6: _t->action_exit(); break;
+        case 7: _t->action_undo(); break;
+        case 8: _t->action_cut(); break;
+        case 9: _t->action_copy(); break;
+        case 10: _t->action_paste(); break;
+        case 11: _t->action_delete(); break;
+        case 12: _t->action_find(); break;
+        case 13: _t->action_select_all(); break;
+        case 14: _t->action_date_time(); break;
+        case 15: _t->action_font(); break;
+        case 16: _t->action_color(); break;
+        case 17: _t->action_background_color(); break;
+        case 18: _t->action_backgroud_color_edit_text(); break;
+        case 19: _t->action_zoom_in(); break;
+        case 20: _t->action_zoom_out(); break;
+        case 21: _t->action_default_zoom(); break;
+        case 22: _t->action_status_bar(); break;
+        case 23: _t->action_view_help(); break;
+        case 24: _t->action_send_feedback(); break;
+        case 25: _t->action_about_notepad(); break;
+        case 26: _t->findNext((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< Qt::CaseSensitivity(*)>(_a[2]))); break;
+        case 27: _t->findPrevious((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< Qt::CaseSensitivity(*)>(_a[2]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 28)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 28;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 28)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 28;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
